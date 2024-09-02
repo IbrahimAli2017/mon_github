e7d0c467a164e0f916a15c34021d97bb602d9c60
IbrahimAli2017 <tech.solutions.im@gmail.com>
2024-09-02 20:37:56 +0300

    rename variable

--- lec/main.cpp
+++ lec/main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-    for(int i =1;i<=n;i++){
-        for(int j = 1;j<=n;j++){
+    int m;
+    cin>>m;
+    for(int i =1;i<=m;i++){
+        for(int j = 1;j<=m;j++){
             cout<<"* ";
         }
         cout<<endl;
